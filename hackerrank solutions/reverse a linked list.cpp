175b2dcca982fae3a0d5077c5a68eb186f54ef68
charlescookey <49230254+charlescookey@users.noreply.github.com>
2019-10-17 13:56:54 +0100

    reverse a linked list

---
+++ hackerrank solutions/reverse a linked list.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* head) {
+     ofstream fout(getenv("OUTPUT_PATH"));
+    SinglyLinkedListNode *temp = head;
+    SinglyLinkedListNode *before = NULL;
+    SinglyLinkedListNode *after = NULL;
+
+    if(head ==  NULL || head->next == NULL)return head;
+    while(temp != NULL){
+        after = temp->next;
+        temp->next = before;
+        before = temp;
+        temp = after;
+    };
+    head = before;
+    return head;
+}
+
+int main()
