faad152b767b84944b0ff9fb6b1ce98ea6b345f6
charlescookey <49230254+charlescookey@users.noreply.github.com>
2019-10-29 04:22:13 +0100

    Kangaroo

--- hackerrank solutions/Kangaroo.cpp
+++ hackerrank solutions/Kangaroo.cpp
@@ -7,8 +7,7 @@ vector<string> split_string(string);
 
 // Complete the kangaroo function below.
 string kangaroo(int x1, int v1, int x2, int v2) {
-    if(v2>=v1)return "NO";
-    if((x1-x2)%(v2-v1)==0)return "YES";
+    if(v1 > v2 && (x1-x2)%(v2-v1)==0 )return "YES";
     else return "NO";
 }
 
